59cbb1c03359a85acd10d38bfd2c32012aacb05e
angusa <angusa@rpi.edu>
2024-11-10 10:46:24 -0500

    Updated changePassword to work
    -Change Passwor works now on settings.php

--- settings.php
+++ settings.php
@@ -5,11 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="./resources/cityfindr.css">
     <link rel="stylesheet" type="text/css" href="./resources/settings.css">
-
-    <title>Document</title>
+    <title>Change Personal Information</title> <!-- Updated title for clarity -->
 </head>
 <body>
-<nav>
+    <nav>
         <a href="./home.php">Home</a>
         <a href="./events.php">Events</a>
         <a href="./organizations.php">Organizations</a>
@@ -18,27 +17,32 @@
     </nav>
 
     <h2>Change Personal Information</h2>
+
     <h3>Change Password</h3>
-    <form>
-        <input type="oldPassword" placeholder="Enter Old Password" required>
-        <input type="newPassword" placeholder="Enter New Password" required>
-        <input type="submit" placeholder="Submit">
+    <form id="changePasswordForm" action="./resources/php/change_password.php" method="POST">
+        <input type="password" name="oldPassword" placeholder="Enter Old Password" required> <!-- Use type="password" -->
+        <input type="password" name="newPassword" placeholder="Enter New Password" required> <!-- Use type="password" -->
+        <input type="submit" value="Submit"> <!-- Use value for submit button -->
     </form>
+
     <h3>Change Location</h3>
-    <form>
-        <input type="addressOne" placeholder="Enter new address one" required>
-        <input type="addressTwo" placeholder="Enter New address two if possible" >
-        <input type="text" id="state" placeholder="Enter your state (if applicable)">
-        <input type="text" id="city" placeholder="Enter your city" required>
-        <input type="text" id="postalCode" placeholder="Enter your postal code" required>
-        <input type="text" id="country" placeholder="Enter your country" required>
-        <input type="submit" placeholder="Submit">
+    <form action="./resources/php/change_location.php" method="POST"> <!-- Added action and method -->
+        <input type="text" name="addressOne" placeholder="Enter new address one" required> <!-- Use type="text" and name attributes -->
+        <input type="text" name="addressTwo" placeholder="Enter New address two if possible">
+        <input type="text" name="state" placeholder="Enter your state (if applicable)">
+        <input type="text" name="city" placeholder="Enter your city" required>
+        <input type="text" name="postalCode" placeholder="Enter your postal code" required>
+        <input type="text" name="country" placeholder="Enter your country" required>
+        <input type="submit" value="Submit"> <!-- Use value for submit button -->
     </form>
+
     <h3>Delete Account</h3>
-    <form>
-        <input type="username" placeholder="Enter username" required>
-        <input type="password" placeholder="Enter password" required>
-        <input type="submit" placeholder="submit">
+    <form action="./resources/php/delete_account.php" method="POST"> <!-- Added action and method -->
+        <input type="text" name="username" placeholder="Enter username" required> <!-- Use type="text" and name attributes -->
+        <input type="password" name="password" placeholder="Enter password" required> <!-- Use type="password" -->
+        <input type="submit" value="Submit"> <!-- Use value for submit button -->
     </form>
+
+    <script src="./resources/settings/settings.js"></script>
 </body>
 </html>
